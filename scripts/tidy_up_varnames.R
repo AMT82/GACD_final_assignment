9efe9be88a50cdbd1a533741d85e6d4ad85846ac
AMT82 <antel.it@gmail.com>
2017-10-24 13:43:15 +1100

    Delete tidy_up_varnames.R

--- scripts/tidy_up_varnames.R
+++
@@ -1,5 +0,0 @@
-tidy_up <- function(feat=features, fulldata=fullData) {
-        clean_features <- gsub("\\(|\\)|-|,","", features$V2)
-        colnames(fullData) <- c("subject", "activity", clean_features)
-        return(fulldata)
-        }
