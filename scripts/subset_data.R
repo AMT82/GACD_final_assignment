53f52a4ac6296d233d0671046168d1a5edf089ec
AMT82 <antel.it@gmail.com>
2017-10-24 13:42:57 +1100

    Delete subset_data.R

--- scripts/subset_data.R
+++
@@ -1,7 +0,0 @@
-subset <- function(fulldata=fullData) {
-        library(dplyr)
-        ## Remove duplicate variable names
-        fulldata <- fulldata[ , !duplicated(colnames(fulldata))]
-        ## Select columns subject, activity, and any with mean or std
-        return(select(fulldata, subject, activity, matches("mean|std")))
-}
