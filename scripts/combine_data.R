b27d21bc117ce908ddd483d020af527849b6bfe8
AMT82 <antel.it@gmail.com>
2017-10-24 13:42:39 +1100

    Delete combine_data.R

--- scripts/combine_data.R
+++
@@ -1,6 +0,0 @@
-combine <- function(subjecttrain=subject_train, ytrain=y_train, Xtrain=X_train,
-                    subjecttest=subject_test, ytest=y_test, Xtest=X_test) {
-        trainData <- cbind(subjecttrain, ytrain, Xtrain)
-        testData <- cbind(subjecttest, ytest, Xtest)
-        return(rbind(trainData, testData))
-}
